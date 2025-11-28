5a22c2fcdbdc97a84caf795df55904ca9158d85b
Pratik K <pratikkoradur@gmail.com>
2025-11-28 11:31:28 +0530

    layout

---
+++ src/pages/user/ParkingLayout.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ParkingLayout = () => {
+  return (
+    <div>
+      <h1>Parking Layout</h1>
+      {/* Parking layout will be implemented here and also add the image*/}
+    </div>
+  );
+};
+
+export default ParkingLayout;
